17ec7d1e7942fa859f8d5b4afcf9a588644ae933
Serge Koba <s.koba@mobidev.biz>
2017-11-21 17:01:34 +0200

    show secret code dialog

--- src/components/Houses.vue
+++ src/components/Houses.vue
@@ -18,18 +18,26 @@
               <div>{{ house.address }}</div>
             </div>
           </v-card-title>
+
           <v-card-actions>
-            <v-btn flat color="orange">
+            <v-btn flat color="orange" @click.native="showSettings(house)">
               <v-icon>settings</v-icon>
             </v-btn>
             <v-btn flat color="orange" @click="showEditDialog(house)">Edit</v-btn>
             <v-btn flat color="red" @click="deleteHouse(house)">Remove</v-btn>
           </v-card-actions>
+          <v-slide-y-transition>
+            <v-card-text v-show="house.showSettings" class="grey darken-1">
+              <h3>Key</h3>
+              {{ house.key }}
+              <v-btn color="primary">Dashboard</v-btn>
+            </v-card-text>
+          </v-slide-y-transition>
         </v-card>
       </v-flex>
     </v-layout>
 
-    <v-dialog v-model="add_dialog" persistent max-width="500px">
+    <v-dialog v-model="addDialog" persistent max-width="500px">
       <v-card>
         <v-card-title class="headline">{{ dialog_header }}</v-card-title>
         <v-card-text>
@@ -52,8 +60,26 @@
         </v-card-text>
         <v-card-actions>
           <v-spacer></v-spacer>
-          <v-btn class="green--text darken-1" flat="flat" @click.native="add_dialog = false">Cancel</v-btn>
+          <v-btn class="green--text darken-1" flat="flat" @click.native="addDialog = false">Cancel</v-btn>
           <v-btn class="green--text darken-1" flat="flat" @click.native="saveHouse">Save</v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
+    <v-dialog v-model="secretDialog" persistent max-width="700px">
+      <v-card>
+        <v-card-title class="headline">Secret code</v-card-title>
+        <v-card-text>
+          <h3>Key</h3>
+          {{ house_key }}
+          <h3>Secret</h3>
+          {{ secret }}
+          <v-alert color="error" icon="warning" value="true">
+            Write down this secret! It won't be shown again! It is required to connect devices to house!
+          </v-alert>
+        </v-card-text>
+        <v-card-actions>
+          <v-spacer></v-spacer>
+          <v-btn class="green--text darken-1" flat="flat" @click.native="secretDialog = false">Close</v-btn>
         </v-card-actions>
       </v-card>
     </v-dialog>
@@ -74,9 +100,12 @@
         houses: [],
         title: '',
         address: '',
+        secret: '',
+        house_key: '',
         dialog_header: '',
         edit_id: null,
-        add_dialog: false
+        addDialog: false,
+        secretDialog: false
       }
     },
     localStorage: {
@@ -94,18 +123,21 @@
         this.edit_id = null
         this.title = ''
         this.address = ''
-        this.add_dialog = true
+        this.addDialog = true
       },
       showEditDialog(house) {
         this.dialog_header = 'Edit House'
         this.edit_id = house.id
         this.title = house.title
         this.address = house.address
-        this.add_dialog = true
+        this.addDialog = true
       },
       getHouses() {
         this.$http.get('/houses', {headers: {'Authorization': 'Bearer ' + this.$localStorage.get('userToken')}}).then(response => {
           this.houses = response.body
+          for (let i = 0; i < this.houses.length; i++) {
+            this.houses[i].showSettings = false
+          }
         }, response => {
           this.$emit('show-error', ['Server error'])
         })
@@ -120,14 +152,17 @@
           if (this.edit_id === null) {
             this.$http.post('/houses', formData, {headers: {'Authorization': 'Bearer ' + this.$localStorage.get('userToken')}}).then(response => {
               this.getHouses()
-              this.add_dialog = false
+              this.addDialog = false
+              this.house_key = response.body.key
+              this.secret = response.body.secret
+              this.secretDialog = true
             }, response => {
               this.$emit('show-error', response.body)
             });
           } else {
             this.$http.put('/houses/' + this.edit_id, formData, {headers: {'Authorization': 'Bearer ' + this.$localStorage.get('userToken')}}).then(response => {
               this.getHouses()
-              this.add_dialog = false
+              this.addDialog = false
             }, response => {
               this.$emit('show-error', response.body)
             });
@@ -143,6 +178,14 @@
             this.$emit('show-error', ['Server error'])
           })
         }
+      },
+      showSettings(house) {
+        for (let i = 0; i < this.houses.length; i++) {
+          if (this.houses[i].id == house.id) {
+            this.houses[i].showSettings = !house.showSettings
+          }
+        }
+        this.$forceUpdate()
       }
     },
     computed: {
